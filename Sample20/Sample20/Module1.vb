e5691bbd29c7e341601f0cef54d810fb5b51c375
KenInoue1911 <inouetkrk3@gamail.com>
2021-10-12 19:12:01 +0900

    10/12

---
+++ Sample20/Sample20/Module1.vb
@@ -0,0 +1,28 @@
+﻿Module Module1
+
+    Sub Mycomp()
+
+        Dim x As Integer
+        Dim y As Integer
+        Dim z As Integer
+
+        Console.Write("整数値を入力してください。")
+        x = Integer.Parse(Console.ReadLine())
+        Console.Write("整数値を入力してください。")
+        y = Integer.Parse(Console.ReadLine())
+
+        If x > y Then
+            z = x
+        Else
+            z = y
+
+        End If
+
+        Console.WriteLine("大きい値は" & z & "です。")
+
+    End Sub
+    Sub Main()
+        Mycomp()
+    End Sub
+
+End Module
