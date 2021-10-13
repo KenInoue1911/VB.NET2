59a099329b2e20db2fe6bd75c391a8d0f091ad5e
KenInoue1911 <inouetkrk3@gamail.com>
2021-10-13 19:49:44 +0900

    10/13

---
+++ Xsample1/Xsample1/Class1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Class1
+    Dim name As String
+    Dim score As Integer
+
+    Sub SetData(tmpName As String, tmpScore As Integer)
+
+        name = tmpName
+        score = tmpScore
+
+    End Sub
+
+    Function disp() As String
+        Return "名前：" & name & "点数：" & score
+    End Function
+End Class
---
+++ Xsample1/Xsample1/Module1.vb
@@ -0,0 +1,17 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim student1 As Class1 = New Class1()
+        Dim student2 As Class1 = New Class1()
+        Dim student3 As Class1 = New Class1()
+
+        student1.SetData("鈴木一郎：", 45)
+        student2.SetData("山田花子：", 90)
+        student3.SetData("田中次郎：", 70)
+
+        Console.WriteLine(student1.disp())
+        Console.WriteLine(student2.disp())
+        Console.WriteLine(student3.disp())
+    End Sub
+
+End Module
